a8717d11f7cc5bbbabec17001d1c9a25247d1f68
Manuelpv17 <manuelpv17@outlook.com>
2020-12-19 07:56:15 -0500

    Fix out of index key

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
-    if boxes == None:
+    if boxes is None:
         return False
     elif len(boxes) == 0:
         return True
+
+    box_len = len(boxes)
 
     keyArray = [0]
     i = 0
     while(len(keyArray) > i):
         for elem in boxes[keyArray[i]]:
-            if elem not in keyArray:
+            if elem not in keyArray and elem < box_len:
                 keyArray.append(elem)
+        print(keyArray)
         i += 1
 
     if i == len(boxes):
