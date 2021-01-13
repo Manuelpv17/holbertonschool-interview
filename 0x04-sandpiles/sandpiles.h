4a2d4bd367813da25cd4bb6c459d1faa5587abbb
Manuelpv17 <manuelpv17@outlook.com>
2021-01-13 08:33:43 -0500

    Add sum two sandpiles

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef _SANDPILES_
+#define _SANDPILES_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_g(int grid[3][3]);
+
+#endif
