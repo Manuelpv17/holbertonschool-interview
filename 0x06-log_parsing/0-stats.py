c9febbb17a1f357ccd03b58334994a1849f94ee9
Manuelpv17 <manuelpv17@outlook.com>
2021-01-31 15:50:38 -0500

    Add log parsing in python

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Log parsing """
+import sys
+
+
+def print_stats(total_size, status_codes):
+    """ Helper function. Print right format """
+    codes = ["200", "301", "400",  "401", "403", "404", "405", "500"]
+    print("File size: {}".format(total_size))
+    for code in codes:
+        if status_codes[code] != 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+codes_count = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+total_size = 0
+line_num = 0
+
+try:
+    for line in sys.stdin:
+        line_num += 1
+        words = line.split()
+        total_size += int(words[-1])
+        if words[-2] in codes_count:
+            codes_count[words[-2]] += 1
+
+        if (line_num % 10 == 0 and line_num != 0):
+            print_stats(total_size, codes_count)
+except KeyboardInterrupt:
+    print_stats(total_size, codes_count)
+    raise
