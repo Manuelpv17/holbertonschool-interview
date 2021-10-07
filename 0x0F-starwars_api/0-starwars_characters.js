b118b5ca4c9018c820b11dcba531adaf9e0e8c7c
Manuelpv17 <manuelpv17@outlook.com>
2021-10-07 18:15:06 -0500

    Add 0x0F. Star Wars API

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const urlMovie = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(urlMovie, async function (error, response, body) {
