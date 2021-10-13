fc7cb69644269d22f98b1544f9396a59dd3f0f83
Manuelpv17 <manuelpv17@outlook.com>
2021-10-13 06:37:03 -0500

    Add merge sort project

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void top_down_merge(int *array, int *copy, size_t low, size_t high);
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
 
