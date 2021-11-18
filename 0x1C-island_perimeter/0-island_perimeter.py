f0cb5d46fbc74f65c26c469ec05a659c5c2511f0
Manuelpv17 <manuelpv17@outlook.com>
2021-11-18 08:03:56 -0500

    Add 0x1C. Island Perimeter

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,17 @@
+""" 0x1C. Island Perimeter """
+
+
+def island_perimeter(grid):
+    perimeter = 0
+    for row_index, row in enumerate(grid):
+        for col_index, elem in enumerate(row):
+            if elem == 1:
+                if row_index == 0 or grid[row_index - 1][col_index] == 0:
+                    perimeter += 1
+                if row_index == len(grid) - 1 or grid[row_index + 1][col_index] == 0:
+                    perimeter += 1
+                if col_index == 0 or grid[row_index][col_index - 1] == 0:
+                    perimeter += 1
+                if col_index == len(grid[row_index]) - 1 or grid[row_index][col_index + 1] == 0:
+                    perimeter += 1
+    return perimeter
