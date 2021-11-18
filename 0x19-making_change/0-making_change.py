75e847d3103c7f2524bc835162c6ec1bc63fe6bc
Manuelpv17 <manuelpv17@outlook.com>
2021-11-18 18:57:15 -0500

    Add making change

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" 0x19. Making Change """
+
+
+def makeChange(coins, total):
+    """ 0x19. Making Change """
+
+    count = 0
+    index = 0
+    length = len(coins)
+    coins.sort(reverse=True)
+
+    while total > 0:
+        if coins[index] <= total:
+            total -= coins[index]
+            count += 1
+        else:
+            index += 1
+            if index >= length:
+                return -1
+
+    return count
